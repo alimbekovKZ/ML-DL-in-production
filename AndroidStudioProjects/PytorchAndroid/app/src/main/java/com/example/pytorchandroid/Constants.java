4ecae8b6694c2da7a5fe1c23ee75e9c462aa8d4c
RENAT ALIMBEKOV <alimbekovr@hotmail.com>
2021-03-16 12:20:01 +0600

    android

---
+++ AndroidStudioProjects/PytorchAndroid/app/src/main/java/com/example/pytorchandroid/Constants.java
@@ -0,0 +1,12 @@
+package com.example.pytorchandroid;
+
+public class Constants {
+
+        public static String[] IMAGENET_CLASSES = new String[]{
+                "healthy",
+                "multiple_diseases",
+                "rust",
+                "scab"
+        };
+}
+
